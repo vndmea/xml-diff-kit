817cae3910f864843c9bd548bae45f246e1f4f4e
vndmea <vndmea@gmail.com>
2026-05-24 11:35:29 +0800

    feat: enhance markdown diff formatter

--- src/format.ts
+++ src/format.ts
@@ -1,4 +1,5 @@
-import type { DiffSummaryItem, FormatDiffOptions, XmlDiffOp } from './types.js';
+import { serializeXml } from './serialize.js';
+import type { DiffSummaryItem, FormatDiffOptions, XmlDiffOp, XmlNode } from './types.js';
 
 export function formatDiff(ops: XmlDiffOp[], options: FormatDiffOptions & { format: 'markdown' }): string;
 export function formatDiff(ops: XmlDiffOp[], options?: FormatDiffOptions): DiffSummaryItem[];
@@ -6,13 +7,127 @@ export function formatDiff(
   ops: XmlDiffOp[],
   options: FormatDiffOptions = {},
 ): DiffSummaryItem[] | string {
-  const summary = ops.map(toSummaryItem);
-
   if (options.format === 'markdown') {
-    return summary.map((item) => `- ${item.message}`).join('\n');
+    return toMarkdown(ops);
   }
 
-  return summary;
+  return ops.map(toSummaryItem);
+}
+
+function toMarkdown(ops: XmlDiffOp[]): string {
+  if (ops.length === 0) {
+    return '# XML Diff\n\nNo XML differences.';
+  }
+
+  return [
+    '# XML Diff',
+    '',
+    `Total changes: ${ops.length}`,
+    '',
+    ...ops.flatMap((op, index) => formatMarkdownOp(op, index + 1)),
+  ].join('\n');
+}
+
+function formatMarkdownOp(op: XmlDiffOp, index: number): string[] {
+  switch (op.op) {
+    case 'addNode':
+      return [
+        `## ${index}. Added node`,
+        '',
+        `- Path: \`${op.path}\``,
+        '',
+        codeBlock('xml', serializeForMarkdown(op.value)),
+        '',
+      ];
+
+    case 'removeNode':
+      return [
+        `## ${index}. Removed node`,
+        '',
+        `- Path: \`${op.path}\``,
+        '',
+        codeBlock('xml', serializeForMarkdown(op.oldValue)),
+        '',
+      ];
+
+    case 'replaceNode':
+      return [
+        `## ${index}. Replaced node`,
+        '',
+        `- Path: \`${op.path}\``,
+        '',
+        '**Before**',
+        '',
+        codeBlock('xml', serializeForMarkdown(op.oldValue)),
+        '',
+        '**After**',
+        '',
+        codeBlock('xml', serializeForMarkdown(op.newValue)),
+        '',
+      ];
+
+    case 'moveNode':
+      return [
+        `## ${index}. Moved node`,
+        '',
+        `- From: \`${op.fromPath}\``,
+        `- To: \`${op.toPath}\``,
+        '',
+        codeBlock('xml', serializeForMarkdown(op.value)),
+        '',
+      ];
+
+    case 'replaceText':
+      return [
+        `## ${index}. Changed text`,
+        '',
+        `- Path: \`${op.path}\``,
+        '',
+        '**Before**',
+        '',
+        codeBlock('text', op.oldValue),
+        '',
+        '**After**',
+        '',
+        codeBlock('text', op.newValue),
+        '',
+        '**Text segments**',
+        '',
+        ...op.segments.map((segment) => `- ${segment.type}: \`${escapeInlineCode(segment.text)}\``),
+        '',
+      ];
+
+    case 'addAttr':
+      return [
+        `## ${index}. Added attribute`,
+        '',
+        `- Path: \`${op.path}\``,
+        `- Name: \`${op.name}\``,
+        `- Value: \`${escapeInlineCode(op.value)}\``,
+        '',
+      ];
+
+    case 'updateAttr':
+      return [
+        `## ${index}. Updated attribute`,
+        '',
+        `- Path: \`${op.path}\``,
+        `- Name: \`${op.name}\``,
+        `- Before: \`${escapeInlineCode(op.oldValue)}\``,
+        `- After: \`${escapeInlineCode(op.newValue)}\``,
+        '',
+      ];
+
+    case 'removeAttr':
+      return [
+        `## ${index}. Removed attribute`,
+        '',
+        `- Path: \`${op.path}\``,
+        `- Name: \`${op.name}\``,
+        `- Old value: \`${escapeInlineCode(op.oldValue)}\``,
+        '',
+      ];
+  }
 }
 
 function toSummaryItem(op: XmlDiffOp): DiffSummaryItem {
@@ -85,4 +200,26 @@ function toSummaryItem(op: XmlDiffOp): DiffSummaryItem {
         before: op.oldValue,
       };
   }
+}
+
+function serializeForMarkdown(node: XmlNode): string {
+  if (node.type === 'text' || node.type === 'comment') {
+    return serializeXml(node);
+  }
+
+  return serializeXml(node, {
+    pretty: true,
+  });
+}
+
+function codeBlock(language: string, value: string): string {
+  return `\`\`\`${language}\n${escapeCodeFence(value)}\n\`\`\``;
+}
+
+function escapeCodeFence(value: string): string {
+  return value.replaceAll('```', '`\u200b``');
+}
+
+function escapeInlineCode(value: string): string {
+  return value.replaceAll('`', '\\`');
 }
